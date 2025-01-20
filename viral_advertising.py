8a834512eef655fd0af3dcb1acb404539948ccec
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2025-01-20 21:19:14 +0500

    Create viral_advertising.py

---
+++ viral_advertising.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    cumulative = 0
+    likes = 0
+    shared = 5
+    
+    
+    for i in range(n):
+        
+        likes = math.floor(shared/2)
+        cumulative += likes
+        shared = likes*3
+        
+    
+    return cumulative
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
