8865a254d597cee9c06f6de224bd2b41b9c9f47b
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2025-01-23 23:47:21 +0500

    Create permutation_equation.py

---
+++ permutation_equation.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'permutationEquation' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY p as parameter.
+#
+
+def permutationEquation(p):
+    y = []
+    z=[]
+    z = sorted(p)
+    for i in range(len(p)):
+        
+        a = p.index(z[i])
+        b = p.index(a) +1
+        y.append(b)
+    
+    return y
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
