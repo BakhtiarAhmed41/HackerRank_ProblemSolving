7a8c2ef6ec0871e70a0ae30a0e87c827be71b182
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2025-05-04 00:10:17 +0500

    Update library_fine.py

--- library_fine.py
+++ library_fine.py
@@ -21,6 +21,24 @@ import sys
 
 def libraryFine(d1, m1, y1, d2, m2, y2):
     # Write your code here
+    fine= 0
+    
+        
+    if(d1 > d2 and m1 == m2 and y1 == y2):
+        fine = 15*(d1-d2)
+        return fine
+    
+    elif(m1 > m2 and y1 == y2):
+        fine = 500*(m1-m2)
+        return fine
+    
+    elif(y1 > y2):
+        fine = 10000
+        return fine
+        
+    else:
+        return fine
+    
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
