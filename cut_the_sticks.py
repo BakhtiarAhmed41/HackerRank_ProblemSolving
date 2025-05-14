97a709fb2f9c6036d0c9a8adec1ea4dacfa1a70f
Bakhtiar Ahmed <114367140+BakhtiarAhmed41@users.noreply.github.com>
2025-05-14 20:22:47 +0500

    Update and rename new_question.py to cut_the_sticks.py

---
+++ cut_the_sticks.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr):
+    sticks = []
+    arr.sort()
+    
+    while arr:
+        sticks.append(len(arr))
+        min_val = arr[0]
+        arr = [x - min_val for x in arr if x - min_val > 0]
+    
+    return sticks
+ 
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
